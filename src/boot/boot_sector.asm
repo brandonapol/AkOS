35fd42ba16369dccc8d63d4172224e08848456dc
Brandon Apol <brandonapol@cedarville.edu>
2022-07-13 14:19:48 -0400

    finished basic BIOS boot instructions

--- src/boot/boot_sector.asm
+++ src/boot/boot_sector.asm
@@ -10,4 +10,25 @@
     mov dl, [ BOOT_DRIVE ]
     call disk_load 
 
-    ; finish from page 33
+   mov dx , [0x9000 ]     ; print first loaded word (declared at bottom)
+   call print_hex 
+
+   mov dx [0x9000 + 512]  ; print second loaded word (second sector)
+   call print_hex 
+
+   jmp $                  ; loop 
+   % include "../helpers/print_hex.asm"
+   % include "../helpers/print.asm"
+   % include "disk.asm"
+
+   BOOT_DRIVE: db 0
+
+   times 510-($-$$) db 0
+   dw 0xaa55
+
+   ; We know that BIOS will load only the first 512 - byte sector from the disk ,
+   ; so if we purposely add a few more sectors to our code by repeating some
+   ; familiar numbers , we can prove to ourselves that we actually loaded those
+   ; additional two sectors from the disk we booted from.
+   times 256 dw 0 xdada
+   times 256 dw 0 xface
