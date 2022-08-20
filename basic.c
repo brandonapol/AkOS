78a76b54208585742b33c4153ffd0e9190e33496
Brandon Apol <brandonapol@cedarville.edu>
2022-08-20 14:40:16 -0400

    started kernel

--- basic.c
+++ basic.c
@@ -2,4 +2,9 @@ int my_function () {
      return 0xbaba ;
 }
 
+// int my_function () {
+//      int my_var = 0 xbaba ;
+//      return my_var ;
+// }
+
 // $ld -o basic.bin -Ttext 0x0 --oformat binary basic.o
